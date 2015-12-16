ad8099c1be864d9b03c57b90cd7a7dc0aa7e9975
Craig Campbell <craig.campbell8@gmail.com>
2015-12-16 11:18:28 -0800

    add httpBasic test, gulpfile, and dependencies

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+
+gulp.task('default', ['lint', 'test']);
+
+gulp.task('lint', function() {
+  gulp.src(['gulpfile.js', 'server.js', 'test/**/*test.js', 'lib/**/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', function() {
+  gulp.src('./test/**/*test.js')
+  .pipe(mocha({reporter: 'nyan'}));
+});
