99e147a5d859317f96900ed44f91765de8ba25d7
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-12-17 13:15:56 -0800

    added tests for the sign in route

--- test/router_test.js
+++ test/router_test.js
@@ -7,6 +7,11 @@ require(__dirname + '/server/server.js');
 
 
 describe('authentication routes - sign up', function() {
+
+  afterEach('drop the database', function() {
+    //insert code to drop the database
+  });
+
   it('should be able to create a user', function(done) {
     chai.request('localhost:3000/api')
       .post('/signup')
@@ -15,13 +20,50 @@ describe('authentication routes - sign up', function() {
         expect(err).to.eql(null);
         expect(res.body.token).to.have.length.above(0);
         done();
-      })
+      });
   });
 
-  it('should be able to error out correctly');
+  before('create test user that will cause a "user already exists" error', function(done) {
+    chai.request('localhost:3000/api')
+      .post('/signup')
+      .send({username: 'testuser', password: '123'})
+      .end();
+      done();
+  });
+
+  it('it should throw an error because user already exists', function(done) {
+    chai.request('localhost:3000/api')
+      .post('/signup')
+      .send({username: 'testuser', password: '123'})
+      .end(function(err, res) {
+        expect(err).to.eql('User already exists');
+        //or whatever it is the error should equal
+        done();
+      });
+  });
 
   describe('authentication routes - sign in', function() {
-    it('should be able to sign in with a token');
+
+    before('create a user for it to sign in to', function(done) {
+      chai.request('locahost:3000/api')
+        .post('/signup')
+        .send({username: 'testuser2', password: '1234'})
+        .end()
+        done();
+    })
+
+    it('should be able to sign in to an existing user', function() {
+      chai.request('localhost:3000/api')
+        .get('/signin')
+        .auth('testuser2', '1234')
+        .end(function(err, res) {
+          expect(err).to.eql(null);
+          expect(res.body.token).to.have.length.above(0);
+          done();
+        });
+    });
+
+    it('should be able to authenticate with a token');
   });
 });
 
