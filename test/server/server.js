1b3479234f442d9df1bcb1c495eb5cb02cc0b9ed
Craig Campbell <craig.campbell8@gmail.com>
2015-12-20 13:58:01 -0800

    role based auth working 3 ways

--- test/server/server.js
+++ test/server/server.js
@@ -2,12 +2,12 @@ var app = require('express')();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var customRoles = ['admin', 'unicorn']; // this is who is authorized to proceed on a path with roleAuth middleware
+var customRoles = ['admin', 'unicorn', 'butterfly']; // this is who is authorized to proceed on a path with roleAuth middleware
 
 // server dev will write this function to obtain the user's role. Then the  array of authorized roles and the user's role obtained by this function must be passed into someFunc as the last line of the function
 var getUserRole = function(req, res, checkAuthStatus){
   var userRole;
-  userRole = req.body.role || 'regularUser'; // this is supplied by the user, and prob should be stored on user resource
+  userRole = req.user.roles[0]; // this is supplied by the user, and prob should be stored on user resource
   checkAuthStatus(customRoles, userRole);
 };
 
@@ -19,7 +19,7 @@ var authenticat = new Authenticat(connection);
 app.use('/api', authenticat.router);
 
 app.get('/secret', bodyParser.json(), authenticat.tokenAuth, authenticat.roleAuth(customRoles, getUserRole), function(req, res) {
-  res.send('success!  Hello ' + req.user);
+  res.send('success!  Hello ' + req.user.username);
 });
 
 app.listen(3000, function() {
