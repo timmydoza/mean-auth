042e51831916dde0e8aed4b57ab6c3fdb9717aa1
Craig Campbell <craig.campbell8@gmail.com>
2015-12-19 21:21:11 -0800

    add body parser to test server /secret route

--- test/server/server.js
+++ test/server/server.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var connection = mongoose.createConnection('mongodb://localhost/whatever');
 var Authenticat = require(__dirname + '/../../index');
@@ -6,7 +7,7 @@ var authenticat = new Authenticat(connection);
 
 app.use('/api', authenticat.router);
 
-app.get('/secret', authenticat.tokenAuth, function(req, res) {
+app.get('/secret', bodyParser.json(), authenticat.tokenAuth, function(req, res) {
   res.send('success!  Hello ' + req.user);
 });
 
