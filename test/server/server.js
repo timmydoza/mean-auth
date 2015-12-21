753972c5e47a9d2b82fd39ec52ba7eee7accfae9
Craig Campbell <craig.campbell8@gmail.com>
2015-12-21 01:29:00 -0800

    fix test server to have roleAuth

--- test/server/server.js
+++ test/server/server.js
@@ -1,16 +1,25 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+var customRoles = ['admin', 'unicorn', 'butterfly'];
+
+var getUserRole = function(req, res, checkAuthStatus){
+  var userRole;
+  userRole = req.user.roles[0];
+  checkAuthStatus(customRoles, userRole);
+};
+
 var connection = mongoose.createConnection('mongodb://localhost/whatever');
 var Authenticat = require(__dirname + '/../../index');
 var authenticat = new Authenticat(connection);
 
 app.use('/api', authenticat.router);
 
-app.get('/secret', bodyParser.json(), authenticat.tokenAuth, function(req, res) {
-  res.send('success!  Hello ' + req.user);
+app.get('/secret', bodyParser.json(), authenticat.tokenAuth, authenticat.roleAuth(customRoles, getUserRole), function(req, res) {
+  res.send('success!  Hello ' + req.user.username);
 });
 
 app.listen(3000, function() {
-  console.log('ok');
+  console.log('server up on port 3000');
 });
